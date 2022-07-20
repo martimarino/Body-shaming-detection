acc9e53de77d46884a0ab4c16c23ddcc7c4423ea
rmatrella <r.matrella@studenti.unipi.it>
2022-07-20 18:50:31 +0200

    new application

--- BSblocker_app/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.bsblocker_app {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.twitter4j.core;
-
-
-    opens com.example.bsblocker_app to javafx.fxml;
-    exports com.example.bsblocker_app;
-}

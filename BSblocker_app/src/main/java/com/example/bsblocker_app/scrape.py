c68fd1407c8eaa6128aa0f3104e82aa0785665a8
Martina Marino <45880539+martimarino@users.noreply.github.com>
2022-07-20 18:57:34 +0200

    Delete BSblocker_app directory

--- BSblocker_app/src/main/java/com/example/bsblocker_app/scrape.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import tkinter as tk
-from tkinter import ttk
-from datetime import datetime, timedelta
-last_time = datetime.now() + timedelta(minutes=5)
-print(last_time.strftime('%Y-%m-%d %H:%M:%S'))
-
